74c2fec777d6f7dc1a36d0c725e7b8ffbc6bec51
DayStayyy <daystayyy@gmail.com>
2023-02-25 12:10:58 +0100

    rm bin

--- bin/CreatePicture.py
+++
@@ -1,9 +0,0 @@
-class CreatePicture :
-    
-    def __init__(self, path) :
-        self.pathToAssets = path
-
-
-    def insertImage(self, imageToModify,imageToInsert, x, y):
-        imageToModify.paste(imageToInsert, (x, y), imageToInsert)
-        return imageToModify
